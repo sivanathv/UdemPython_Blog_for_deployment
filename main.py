fb175e38ce597875177e770a0f911dd38499cff4
Acer <acer@gmail.com>
2024-04-20 11:03:10 +0530

    create Procfile

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
+import os
 
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')  # 8BYkEfBA6O6donzWlSihBXox7C0sKR6b
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -44,7 +45,9 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")  # sqlite:///posts.db
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
